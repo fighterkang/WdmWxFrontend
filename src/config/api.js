92d0fb06d02475a6bcb2d0e5c098fdaa651734c3
kyj <1521114896@qq.com>
2018-07-23 20:53:22 +0800

    切换线上接口

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export default {
   // api: 'http://wjpdd.yoflying.com/gzh/?service=',       // 测试环境api接口
-  api: 'http://47.75.0.236/jc_service/', // 测试环境api接口
-  // api: 'http://api.cnjczh.com/jc_service/', // 正式环境api接口
+  // api: 'http://47.75.0.236/jc_service/', // 测试环境api接口
+  api: 'http://api.cnjczh.com/jc_service/', // 正式环境api接口
   bbs: 'http://abc.cnjczh.com/we/?service=',
 }
